a926c08a91642d992de6c10fcac27c432a0d8024
vahe54262 <70031451+vahe54262@users.noreply.github.com>
2021-07-26 23:17:59 +0400

    Add files via upload

---
+++ task.js
@@ -0,0 +1,65 @@
+/*Method map() s for///
+
+function my_Map (item) {
+let newArray = [];
+for(let i = 0; i < my_Map.length; ++i) {
+    newArray.push(item);
+}
+return newArray;
+}
+
+
+console.log(my_Map([1,2,3,4]));
+
+*/
+
+
+
+/*Sobstvenniy Map()
+
+
+function my_Map (item) {
+let newArray = [];
+if(Array.isArray(item)) {
+
+    newArray.push(item);
+}
+return newArray;
+}
+
+
+console.log(my_Map([1,2,3,4]));
+
+*/ 
+
+
+/*Splice method
+
+function tuckIn(x, y) {
+   
+         x.splice(1,0,...y);
+    return x;
+
+
+}
+
+console.log(tuckIn(["Orange", "Banana"], ["Strawberry", "Apple"]));
+
+*/
+
+ /*NUMBER isInteger
+
+            function intWithinBounds(n, lower, upper) {
+	if (!Number.isInteger(n)) {
+		
+		
+	
+	return false;
+}
+return n >= lower && n < upper;
+
+            }
+
+ console.log(intWithinBounds(-5, -10, 6 ));
+
+ */
